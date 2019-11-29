bf3b62eb3c18d690333b45aa95d38d98b51c407e
savaserehemanth <savaserehemanth@gmail.com>
2019-11-29 10:29:40 +0530

    Java Arrays and Matrices and Stacks

--- src/com/hemanthsavasere/stacks/Prefix.java
+++ src/com/hemanthsavasere/stacks/Prefix.java
@@ -1,4 +1,12 @@
 package com.hemanthsavasere.stacks;
 
 public class Prefix {
+    public static String infixToPrefix(String s){
+        String s1 = new StringBuilder(s).reverse().toString();
+        String res = new StringBuilder(Postfix.infixToPostifx(s1)).reverse().toString();
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(infixToPrefix("A+B*C"));
+    }
 }
